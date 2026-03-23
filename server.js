158636a7808f818691d53d57e0840fa443fc5cad
dominikpackton-ai <dominikpackton@gmail.com>
2026-03-23 14:00:26 +0530

    Add server listening on specified PORT

--- server.js
+++ server.js
@@ -5,3 +5,8 @@ app.use(express.static(path.join(__dirname)));
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
 });
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Server running");
+});
