c9e3a08d603876e373c3bcc59bc3dde157e5854f
dominikpackton-ai <dominikpackton@gmail.com>
2026-03-23 14:25:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const express = require("express");
 const axios = require("axios");
 const cheerio = require("cheerio");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
-const path = require("path");
 
 app.use(express.static(path.join(__dirname)));
 
